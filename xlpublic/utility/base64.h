4261c81bd197e84e940dc6b331f8cd8308373fbb
徐力 <466300686@qq.com>
2020-08-28 12:04:25 +0800

    base64

---
+++ xlpublic/utility/base64.h
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Filename         : Base64.h
+// Author           : XULI
+// Created          : 2020-8-28
+// Description      : Base64 加解密
+// ***********************************************************************
+#ifndef BASE64_H
+#define BASE64_H
+#include <string>
+namespace XL {
+	namespace UTILITY
+	{
+		// len == strlen(src), and strlen(dst) >= len * 4 / 3
+		// 加密
+		size_t         base64Encode(const char *src, size_t len, char *dst);
+		size_t         base64Encode(const char *src, size_t len, std::string& dst);
+		size_t         base64Encode(const std::string& src, std::string& dst);
+		std::string    base64Encode(const char *src, size_t len);
+		std::string    base64Encode(const std::string& src);
+
+		// len == strlen(src), and strlen(dst) >= len * 3 / 4
+		// 解密
+		size_t         base64Decode(const char *src, size_t len, char *dst);
+		size_t         base64Decode(const char *src, size_t len, std::string& dst);
+		size_t         base64Decode(const std::string& src, std::string& dst);
+		std::string    base64Decode(const char *src, size_t len);
+		std::string    base64Decode(const std::string& src);
+	}
+}
+
+#endif  /* ZL_BASE64_H */
